ca9e74e0b048c525eee00bb18d6a226034b336e1
Yusup <ereshjumayew@gmail.com>
2025-01-26 21:04:32 +0000

    activated add to excell and create

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -142,37 +142,37 @@ class FrontController extends Controller
             return back()->withErrors($validator)->withInput();
         }
 
-////        spreadsheet write here
-//        Sheets::spreadsheet('1AJahI4bSxV7JXe9TaqGdeZ_adTPR3miP0p67OSBLF50')->sheet('register')->append([[
-//            $this->getNextId() ?? '',
-//            $request->name ?? '',
-//            $request->surname ?? '',
-//            $request->middle_name ?? '',
-//            $request->company_name ?? '',
-//            $request->job ?? '',
-//            \Helper::getCountryName($request->country) ?? '',
-//            $request->number ?? '',
-//            $request->emergency_number ?? '',
-//            $request->email ?? '',
-//            $request->website ?? '',
-//            $request->status ?? '',
-//            $request->visa ?? '',
-//            ]]);
-////            create register here
-//        $register = RegistrationForm::create([
-//            'name'                 => $request->name ?? '',
-//            'surname'              => $request->surname ?? '',
-//            'middle_name'          => $request->middle_name ?? '',
-//            'company_name'         => $request->company_name ?? '',
-//            'job'                  => $request->job ?? '',
-//            'country'              => \Helper::getCountryName($request->country) ?? '',
-//            'number'               => $request->number ?? '',
-//            'emergency_number'     => $request->emergency_number ?? '',
-//            'email'                => $request->email ?? '',
-//            'website'              => $request->website ?? '',
-//            'status'               => $request->status ?? '',
-//            'visa'                 => $request->visa ?? '',
-//        ]);
+//        spreadsheet write here
+        Sheets::spreadsheet('1AJahI4bSxV7JXe9TaqGdeZ_adTPR3miP0p67OSBLF50')->sheet('register')->append([[
+            $this->getNextId() ?? '',
+            $request->name ?? '',
+            $request->surname ?? '',
+            $request->middle_name ?? '',
+            $request->company_name ?? '',
+            $request->job ?? '',
+            \Helper::getCountryName($request->country) ?? '',
+            $request->number ?? '',
+            $request->emergency_number ?? '',
+            $request->email ?? '',
+            $request->website ?? '',
+            $request->status ?? '',
+            $request->visa ?? '',
+            ]]);
+//            create register here
+        $register = RegistrationForm::create([
+            'name'                 => $request->name ?? '',
+            'surname'              => $request->surname ?? '',
+            'middle_name'          => $request->middle_name ?? '',
+            'company_name'         => $request->company_name ?? '',
+            'job'                  => $request->job ?? '',
+            'country'              => \Helper::getCountryName($request->country) ?? '',
+            'number'               => $request->number ?? '',
+            'emergency_number'     => $request->emergency_number ?? '',
+            'email'                => $request->email ?? '',
+            'website'              => $request->website ?? '',
+            'status'               => $request->status ?? '',
+            'visa'                 => $request->visa ?? '',
+        ]);
 
 
 //        $directoryPath = public_path('images/registration');
